5511f41b524e40494614665f3c1f6530571aa854
Shalvah Adebayo <Shalvah@users.noreply.github.com>
2016-11-19 16:23:30 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,9 +18,9 @@
 			</div>
 		</header>
 		<section class="cta">
-			<div style="width: 30%;">
+			<div style="width: 40%;">
 				<div class="cta-caption">
-					<h1>"Your team has more than 10,000 messages in its archive, so although there are older messages than are shown below, you can't see them." Sad, right? That's why Linxer is here-so you can save the important stuff.</h1>
+					<h1>"Your team has more than 10,000 messages in its archive, so although there are older messages than are shown below, you can't see them."</h1> <h2>Sad, right? That's why Linxer is here-so you can save the important stuff.</h2>
 				</div>
 				<div class="cta-link">
 					<a href="https://slack.com/oauth/authorize?scope=incoming-webhook,bot&client_id=104593454705.107498116711">
@@ -34,9 +34,6 @@
 		</section>
 		<section class="screenshot">
 			<div class="screenshot-list">
-				<div class="screenshot-list__item">
-					<img src="" alt="">
-				</div>
 				<div class="screenshot-list__item">
 					<img src="" alt="">
 				</div>
