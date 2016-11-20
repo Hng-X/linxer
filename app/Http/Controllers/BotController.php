aa1d83df09661d850ba70992cb87b95321276d27
Shalvah <diakon.ng@gmail.com>
2016-11-21 00:16:10 +0100

    Refactor events verification int middleware

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -2,35 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\EventsMiddleware;
 use Illuminate\Http\Request;
 
 class BotController extends Controller
 {
-    function __construct {
-
+    function __construct()
+    {
+        $this->middleware(EventsMiddleware::class);
     }
 
     //all incoming links from Slack go here
     public function receive() {
-    	ini_set("allow_url_fopen", 1);
 
-    	$post = file_get_contents("php://input");
-    	$data = json_decode($post);
-    	$challenge = $data->challenge;
-       
-       if($challenge) {
-          return verify($challenge);
-       }
-
-    }
-
-//Events API verification
-public function verify($challenge) {
-    	//http_response_code(200);
-    	header("HTTP/1.0 200 OK");
-    	header("Content-Type: application/json");
-    	$confirm = array('challenge' => $challenge);
-
-    	return json_encode($confirm);
     }
 }
