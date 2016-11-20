58b91b0e0017e6f55315613c192c5919e3cd34f8
Shalvah <diakon.ng@gmail.com>
2016-11-20 23:30:28 +0100

    Clean up routes and controllers

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,6 @@ Route::get('/', function () {
     return view('home');
 });
 //Route::get('/links/{team-name}', 'WebController@viewLinks');
-Route::get('/links', function () {
-    return view('listing');
-});
 
 // $router->group(['prefix' => 'auth/slack', 'namespace' => 'Auth'], function ($router) {
 //     $router->get('/callback/user', ['as' =>'auth.slack.callback.user', 'uses' => 'AuthController@handleProviderCallbackUser']);
@@ -25,10 +22,6 @@ Route::get('/links', function () {
 //     $router->get('/', ['as' => 'auth.slack', 'uses' => 'AuthController@redirectToProvider']);
 // });
 	Route::get('/authorize', 'AuthController@handleProviderCallback');
-
-//---- Events API verification route
-Route::post('/verify', 'VerificationController@verify');
-Route::get('/verify', 'VerificationController@verify');
 
 //---- bot events route
 Route::post('/links', 'BotController@receive');
