db30a44881473c76406daabd0ddc04aed72dc4f8
pascalskillz <pascalskillz@gmail.com>
2016-11-20 02:46:41 +0100

    config .env file properly to fix errors

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,10 @@
 Route::get('/', function () {
     return view('home');
 });
-<<<<<<< HEAD
 //Route::get('/links/{team-name}', 'WebController@viewLinks');
-
 Route::get('/links', function () {
     return view('listing');
 });
-=======
-Route::get('/links/{team-name}', 'WebController@viewLinks');
-
 
 // $router->group(['prefix' => 'auth/slack', 'namespace' => 'Auth'], function ($router) {
 //     $router->get('/callback/user', ['as' =>'auth.slack.callback.user', 'uses' => 'AuthController@handleProviderCallbackUser']);
@@ -30,4 +25,3 @@ Route::get('/links/{team-name}', 'WebController@viewLinks');
 //     $router->get('/', ['as' => 'auth.slack', 'uses' => 'AuthController@redirectToProvider']);
 // });
 	Route::get('/authorize', 'AuthController@handleProviderCallback');
->>>>>>> 098a3fc923c06de457a172c3e2cf922e46a5de03
