a5f7b2a93096a3ae7d061dbc6d061a904d37d9ee
Shalvah <diakon.ng@gmail.com>
2016-11-21 00:38:48 +0100

    Add boilerplate code and documentation

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -4,10 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/**
+ * Handles requests through the browser ie at the live site
+ */
 class WebController extends Controller
 {
     public function viewLinks()
     {
-        return view('listing');
+
+        if (isset($_GET["query"])) {
+            //$results=search($teamName, $_GET["query"]);
+            return view('listing', $results);
+        } else {
+            //$results=getAllLinks($teamName);
+            return view('listing');
+        }
+    }
+
+    /** Retrieves all a team's links
+     *
+     * @param $team The name of the team
+     * @param $queryString The query string
+     */
+    public function getAllLinks($team)
+    {
+
+    }
+
+    /** Searches for a team's links matching a given search query
+     *
+     * @param $team The name of the team
+     * @param $queryString The query string
+     */
+    public function search($team, $queryString)
+    {
+
     }
 }
