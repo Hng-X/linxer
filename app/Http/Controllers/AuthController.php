8c3c74876a0e7e9e825fd45a63e4091578ed0250
Shalvah <diakon.ng@gmail.com>
2016-11-21 18:23:36 +0100

    Fix client id error

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
          $code=$_GET['code'];
 $client=new Client();
              $response=$client->request('POST', 'https://slack.com/api/oauth.access',
-['json' => ['client_id' => env('SLACK_CLIENT_ID'),
+['json' => ['client_id' => '104593454705.107498116711',
 'client_secret' => env('SLACK_CLIENT_SECRET'),
 'code' => $code]]);
 $response=json_decode($response->getBody(), true);
