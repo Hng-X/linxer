84aa927a7c948062e59f9cd09688d92cf3a58994
adexot <adekunleoseni47@gmail.com>
2016-11-21 10:02:56 +0100

    made homepage responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,10 +20,11 @@
 				</div>
 			</div>
 		</header>
+
 		<section class="cta">
-			<div style="width: 40%;">
+			<div class="cta-intro">
 				<div class="cta-caption">
-					<h1>"Your team has more than 10,000 messages in its archive, so although there are older messages than are shown below, you can't see them."</h1> <h2>Sad, right? That's why Linxer is here—so you can save the stuff that matters.</h2>
+					<h1>"Your team has more than 10,000 messages in its archive, although there are older messages than are shown below, you can't see them."</h1> <h2>Sad, right? That's why Linxer is here—so you can save the stuff that matters.</h2>
 				</div>
 				<div class="cta-link">
 					<a href="https://slack.com/oauth/authorize?scope=incoming-webhook,bot&client_id=104593454705.107498116711">
@@ -31,10 +32,12 @@
 					</a>
 				</div>
 			</div>
+
 			<div class="cta-img">
-				<p style="width: 350px; height: 350px; visibility: hidden;"></p>
+				
 			</div>
 		</section>
+
 		<section class="screenshot">
 			<div class="screenshot-list">
 				<div class="screenshot-list__item">
@@ -45,9 +48,11 @@
 				</div>
 			</div>
 		</section>
+
+
 		<footer>
 			<p>made with ❤️ by <a href="">hngX</a></p>
 		</footer>
 	</div>
-	</body>
+</body>
 </html>
