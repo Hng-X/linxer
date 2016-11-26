25d06da81476803044fe78be220110778d76420e
Shalvah <diakon.ng@gmail.com>
2016-11-26 20:35:12 +0100

    Verify url and save in db

--- app/Jobs/HandleSlackEvent.php
+++ app/Jobs/HandleSlackEvent.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use App\Models\Credential;
+use App\Models\Link;
 use GuzzleHttp\Client;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
@@ -33,21 +34,39 @@ class HandleSlackEvent implements ShouldQueue
      */
     public function handle()
     {
+        //determine what kind of message this is: add, search, or not for us
         $text = $this->request['event']['text'];
-        $data = [];
-        if ($this->parseText($text)['type'] == 'add') {
-            $data['text'] = "Done! See all your team's links here. :+1:";
-            $data['channel'] = $this->request['event']['channel'];
-            $data['team_id'] = $this->request['team_id'];
-            $data['response_type'] = "saved";
+        $text = $this->getMessageTypeAndParseText($text);
 
-            $response = $this->respond($data);
-            Log::info("Received response:" . print_r($response, true));
+        $data = [];
+        if ($text['type'] == 'add') {
+            //add link to db
+            if ($url = $this->sanitizeAndVerifyUrl($text["link"])) {
+                $attributes = array(
+                    "team_id" => $this->request['team_id'],
+                    "url" => $url,
+                    "user_id" => $this->request['event']['user'],
+                    "channel_id" => $this->request['event']['channel'],
+                    "title" => $this->getTitle($text["link"])
+                );
+                $link = Link::create($attributes);
+
+                //respond
+                $teamName = "";
+                $teamLinksUrl = env('APP_URL') . "/links/" . $this->request['team_id'] . "-" . $teamName;
+                $data['text'] = "Done! :+1: See all your team's links <$teamLinksUrl|here>. ";
+                $data['channel'] = $this->request['event']['channel'];
+                $data['team_id'] = $this->request['team_id'];
+                $data['response_type'] = "saved";
+
+                $response = $this->respond($data);
+                Log::info("Received response:" . print_r($response, true));
+            }
         }
     }
 
 
-    public function parseText($text)
+    public function getMessageTypeAndParseText($text)
     {
         $tokens = explode(' ', $text);
         if ($tokens[0] == "@linxer") {
@@ -61,6 +80,22 @@ class HandleSlackEvent implements ShouldQueue
                     'type' => 'search',
                     'query_terms' => array_slice($tokens, 2));
             }
+        }
+    }
+
+    private function sanitizeAndVerifyUrl($text)
+    {
+        $text = filter_var($text, FILTER_SANITIZE_URL);
+        return filter_var($text, FILTER_VALIDATE_URL);
+    }
+
+    private function getTitle($url)
+    {
+        $str = file_get_contents($url);
+        if (strlen($str) > 0) {
+            $str = trim(preg_replace('/\s+/', ' ', $str)); // supports line breaks inside <title>
+            preg_match("/\<title\>(.*)\<\/title\>/i", $str, $title); // ignore case
+            return $title[1];
         }
     }
 
