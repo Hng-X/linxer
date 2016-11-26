822085979b175261782e37822a0b4f8d7f57a320
Shalvah <diakon.ng@gmail.com>
2016-11-26 20:40:29 +0100

    Update link verification

--- app/Jobs/HandleSlackEvent.php
+++ app/Jobs/HandleSlackEvent.php
@@ -73,7 +73,7 @@ class HandleSlackEvent implements ShouldQueue
             if ($tokens[1] == "add" || $tokens[1] == "save") {
                 return array(
                     'type' => 'add',
-                    'link' => $tokens[2],
+                    'link' => trim($tokens[2], "<>"),
                     'tags' => array_slice($tokens, 3));
             } else if ($tokens[1] == "find" || $tokens[1] == "search") {
                 return array(
