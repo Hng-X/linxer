c2abcacea46002dc102fd38902853b94b5fc8751
Olusola <akinsulereolusola@gmail.com>
2016-12-06 10:59:01 +0100

    Edited Controllers

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
-use App\Models\Link;
+
 /**
  * Handles requests through the browser ie at the live site
  */
@@ -10,11 +11,19 @@ class WebController extends Controller
 {
     public function viewLinks($teamSlug)
     {
-
-        $team = explode('-', $teamSlug);
-        $links = Link::where('team_id', $team[0])->get();
-        return view("listing", ["links" => $links]);
-        return view("listing", ["teamName" => $team[1]]);
+                      $teamName="HngX";
+                      $teamId="Txjrd24";
+                      $query="";
+                      $results=[];
+             //parse $teamId and $teamName from $teamSlug
+        if (isset($_GET["query"])) {
+        $query=$_GET["query"];
+            //$results=search($teamId, $query);
+            return view('listing', ["teamName" => $teamName, "query" => $query, "results" => $results]);
+        } else {
+            //$results=getAllLinks($teamId);
+            return view('listing', ["teamName" => $teamName, "results" => $results]);
+        }
     }
 
     /** Retrieves all a team's links
@@ -24,7 +33,7 @@ class WebController extends Controller
      */
     public function getAllLinks($team)
     {
-    
+
     }
 
     /** Searches for a team's links matching a given search query
