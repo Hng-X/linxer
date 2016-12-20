fa2e95248159711f0dd4a5cf1ab7143da783b611
Olusola <akinsulereolusola@gmail.com>
2016-12-20 15:19:27 +0100

    Added styling to listing page

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="{{ env('APP_URL') }}/css/listing.css" rel="stylesheet" type="text/css" />
+	<link href="/css/listing.css" rel="stylesheet" type="text/css" />
   <link href="/css/home.css" rel="stylesheet" type="text/css" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
@@ -20,7 +20,7 @@
             <h1 class="logo__text">Linxer</h1>
           </div>
           <div class="signin">
-                      <p>Your team's already using Linxer?</p>
+                      <p>Log Out</p>
             <a href="https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team&client_id=104593454705.107498116711&redirect_uri=http://linxer.herokuapp.com/Auth/signin">
               <button class="signin-btn"><span>Log Out From Slack</span></button>
             </a>
@@ -49,28 +49,28 @@
       <!-- Card Section -->
       <div class = "flip3D">
         <div class = "back">
-
+            Links will go here
         </div>
         <div class = "front">
-
+          Links will also go here
         </div>
       </div>
 
       <div class = "flip3D">
         <div class = "back">
-
+            Links will also go here
         </div>
         <div class = "front">
-
+          Links will also go here
         </div>
       </div>
 
       <div class = "flip3D">
         <div class = "back">
-
+          Links will also go here
         </div>
         <div class = "front">
-
+          Links will also go here
         </div>
       </div>
 
