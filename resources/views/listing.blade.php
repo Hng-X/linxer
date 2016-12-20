4f66f04f3d2e5162e980e8c643af9887077f1fc3
Olusola <akinsulereolusola@gmail.com>
2016-12-20 21:42:44 +0100

    Fixed bug

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!--<link href="/css/listing.css" rel="stylesheet" type="text/css" /> -->
+	<link href="/css/listing.css" rel="stylesheet" type="text/css" /> 
   <link href="/css/home.css" rel="stylesheet" type="text/css" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
