f6398002fd1728788392afe9b0f2a40d113094d8
Quadri Lasisi <quad.lasisi@gmail.com>
2016-12-22 11:56:39 +0100

    search implementation (by tags) - test 5.7

--- app/Jobs/HandleSlackEvent.php
+++ app/Jobs/HandleSlackEvent.php
@@ -96,7 +96,7 @@ class HandleSlackEvent implements ShouldQueue
                 $team = $this->request['team_id'];
             
             
-                $check = Link::where('team_id', $team)->get();
+                $check = Link::where('title', $tag_term)->get();
                 //where('team_id', $team)->where('title', 'ILIKE', '%$tag_term%')                         ->
                             //->where('tags', 'ILIKE', '%$tag_term%')   //"ILIKE" is not a typo. it's needed for case-insensitive pattern matching in Postgres.
                                                          
